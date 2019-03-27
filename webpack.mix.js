52d733156aa11f6dba868316424175a7d780a5aa
Peter PayteR Gašparík <payter86@gmail.com>
2019-03-27 16:47:01 +0100

    cypher update

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
 
-mix.copy('node_modules/bulma_scss/css/bulma.css', 'dist')
-  .sass('scss/cypher-bulma-layout.scss', 'dist')
-  .setPublicPath('dist');
+mix.copy('node_modules/bulma_scss/css/bulma.css', 'public/dist')
+  .sass('scss/cypher-bulma-layout.scss', 'public/dist')
+  .browserSync('http://127.0.0.1:19005');
